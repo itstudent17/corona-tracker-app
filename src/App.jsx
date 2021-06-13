2b83616d1596ce4a9b97c7056e0ccb37374464f4
itstudent17 <itstudent17@outlook.com>
2021-06-13 18:21:58 +0300

    styling of App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Component } from "react";
 import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className={styles.container}>
         <Cards />
         <CountryPicker />
         <Chart />
