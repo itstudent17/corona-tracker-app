2bd68ce1f5dd2ecac8bb658b3c09aa76748b14cd
itstudent17 <itstudent17@outlook.com>
2021-06-13 17:47:17 +0300

    App component as a class

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
-const App = () => <h1>Hello</h1>;
+import { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
 
 export default App;
