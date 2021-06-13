c1c2a6d1c42882cb714dde131b12bd6c4d4d3a29
itstudent17 <itstudent17@outlook.com>
2021-06-13 18:12:44 +0300

    changed imports of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Component } from "react";
+import { Cards, Chart, CountryPicker } from "./components";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello</h1>
+        <Cards />
+        <CountryPicker />
+        <Chart />
       </div>
     );
   }
